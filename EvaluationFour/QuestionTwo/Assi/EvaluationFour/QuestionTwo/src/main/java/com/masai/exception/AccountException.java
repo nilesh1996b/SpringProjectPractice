f154bc06a9b98da985e30f7dee220afe34850dee
Nilesh Bind <nileshbind.nb@gmail.com>
2023-03-19 10:57:56 +0530

    Add files via upload

---
+++ EvaluationFour/QuestionTwo/Assi/EvaluationFour/QuestionTwo/src/main/java/com/masai/exception/AccountException.java
@@ -0,0 +1,15 @@
+package com.masai.exception;
+
+public class AccountException extends RuntimeException {
+	
+	public AccountException()
+	{
+		
+	}
+	
+	public AccountException(String message)
+	{
+		super(message);
+	}
+
+}
