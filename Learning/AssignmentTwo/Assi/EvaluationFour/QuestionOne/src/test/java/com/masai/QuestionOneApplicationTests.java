6847b2a2d313c9dd559f9b3ae1288fd180e020f9
Nilesh Bind <nileshbind.nb@gmail.com>
2023-03-25 10:37:33 +0530

    Add files via upload

---
+++ Learning/AssignmentTwo/Assi/EvaluationFour/QuestionOne/src/test/java/com/masai/QuestionOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
