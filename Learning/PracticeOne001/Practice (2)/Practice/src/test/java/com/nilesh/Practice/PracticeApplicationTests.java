cab4c6551e93eabb3abd22f73fec350b74e645c9
Nilesh Bind <nileshbind.nb@gmail.com>
2023-03-31 18:03:24 +0530

    Add files via upload

---
+++ Learning/PracticeOne001/Practice (2)/Practice/src/test/java/com/nilesh/Practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilesh.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
